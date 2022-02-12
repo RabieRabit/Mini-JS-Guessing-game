8a5d1fe2dd00ab97d8d93e3d7c65604382c2e16d
Dylan <dylanschutte10@gmail.com>
2022-02-12 16:36:54 +0200

    Basic game is working

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const start = document.getElementById('start');
+
+start.addEventListener("click", startfunc);
+
+let myNum;
+
+function startfunc(){
+    let max = window.prompt("Choose the max value:");
+    myNum = Math.floor((Math.random() * max) + 1);
+    let guess = window.prompt("Geuss my number!");
+    checkGuess(guess);
+}
+
+function checkGuess(num) {
+    while (myNum != num){
+        num = window.prompt("Guess Again!");
+    }
+    alert("Well Done you did it! The num was " + myNum)
+}
